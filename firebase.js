59be268fc982a34c4513d746fb32af6edc1add92
peppapig450 <nbgenius1@gmail.com>
2024-07-30 17:42:15 -0700

    setup the firebase js properly, exporting firestore

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
 import { firebase } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import firebaseConfig from "./config/firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+export { firestore };
