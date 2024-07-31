b0f580d3a7b5aba057143b53fa6ccdb7c9e91316
peppapig450 <nbgenius1@gmail.com>
2024-07-30 18:51:22 -0700

    finish writing the functions and begin the components

--- app/page.js
+++ app/page.js
@@ -55,10 +55,79 @@ export default function Home() {
     updateInventory();
   }, []);
 
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docref, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+    await updateInventory();
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   // Add component logic here
   return (
-    <Box>
-      <Typography variant="h1">Inventory Management</Typography>
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+    >
+      <Box border={"1px solid #333"}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#ADD8E6"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignContent={"center"}
+        >
+          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
+            Pantry Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
+          {item.map((i) => (
+            <Box
+              key={i}
+              width="100%"
+              height="300px"
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0"}
+            >
+              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+                {
+                  // Capitalize the first letter of the item
+                  i.charAt(0).toUpperCase() + i.slice(1)
+                }
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
