4686267fe02a71616aa2a7dce617259a0f1afe50
peppapig450 <nbgenius1@gmail.com>
2024-07-30 17:57:49 -0700

    setup the const style for our default style, and add a typography box for the top level header

--- app/page.js
+++ app/page.js
@@ -1,60 +1,45 @@
-import { Box, Stack, Typography } from "@mui/material";
+"use client";
 
-const item = [
-  "tomato",
-  "potato",
-  "onion",
-  "garlic",
-  "ginger",
-  "carrot",
-  "kale",
-  "cucumber",
-];
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  Button,
+  Modal,
+  TextField,
+} from "@mui/material";
+import { firestore } from "@/firebase";
+import {
+  collection,
+  docs,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 3,
+};
 
 export default function Home() {
+  // Add component logic here
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display={"flex"}
-      justifyContent={"center"}
-      flexDirection={"column"}
-      alignItems={"center"}
-    >
-      <Box border={"1px solid #333"}>
-        <Box
-          width="800px"
-          height="100px"
-          bgcolor={"#ADD8E6"}
-          display={"flex"}
-          justifyContent={"center"}
-          alignContent={"center"}
-        >
-          <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
-            Pantry Items
-          </Typography>
-        </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
-          {item.map((i) => (
-            <Box
-              key={i}
-              width="100%"
-              height="300px"
-              display={"flex"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              bgcolor={"#f0f0f0"}
-            >
-              <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
-                {
-                  // Capitalize the first letter of the item
-                  i.charAt(0).toUpperCase() + i.slice(1)
-                }
-              </Typography>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
     </Box>
   );
 }
