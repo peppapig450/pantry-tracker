589762ae9d180ffa5745ece792477ca6c24dc0a1
peppapig450 <nbgenius1@gmail.com>
2024-07-30 18:26:59 -0700

    add inventory fetching useing firestore

--- app/page.js
+++ app/page.js
@@ -36,9 +36,24 @@ const style = {
 };
 
 export default function Home() {
-  const [inventory, setInvetory] = useState([]);
+  const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() });
+    });
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
 
   // Add component logic here
   return (
