f4fa569ec46f7f688e08a2bb16176ba9b74997aa
peppapig450 <nbgenius1@gmail.com>
2024-07-30 18:22:07 -0700

    comment out analytics for right now

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import firebaseConfig from "./config/firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 export { firestore };
