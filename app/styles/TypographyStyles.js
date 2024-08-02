84843e15d026850941e97eda7504e3b6ad12611e
peppapig450 <nbgenius1@gmail.com>
2024-08-02 12:53:01 -0700

    move the typography styles to a styles directory

---
+++ app/styles/TypographyStyles.js
@@ -0,0 +1,30 @@
+// Base typography settings for default font
+// TODO: figure out how to install Raleway
+const baseTypography = {
+  fontFamily: "Raleway, Arial, sans-serif",
+};
+
+// Shared typography for headers
+const sharedTypography = {
+  ...baseTypography,
+  h1: {
+    fontFamily: "Playfair Display, serif",
+  },
+  h2: {
+    fontFamily: "Playfair Display, serif",
+  },
+  h3: {
+    fontFamily: "Playfair Display, serif",
+  },
+  h4: {
+    fontFamily: "Playfair Display, serif",
+  },
+  h5: {
+    fontFamily: "Playfair Display, serif",
+  },
+  h6: {
+    fontFamily: "Playfair Display, serif",
+  },
+};
+
+export { sharedTypography };
