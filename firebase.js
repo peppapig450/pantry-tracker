03876a938ed73df4f47d7f6a612b025e5064957b
peppapig450 <nbgenius1@gmail.com>
2024-08-03 09:54:34 -0700

    add auth to firebase for the sign up functionality

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 import firebaseConfig from "./config/firebaseConfig";
 
@@ -7,4 +8,6 @@ import firebaseConfig from "./config/firebaseConfig";
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
-export { firestore };
+const auth = getAuth(app);
+
+export { firestore, auth };
