132d148dfdd3711417b04b51c8992c2788d8084b
peppapig450 <nbgenius1@gmail.com>
2024-08-03 11:15:33 -0700

    incorporate the dark mode switch into the base app bar

--- app/components/AppBars/BaseAppBar.jsx
+++ app/components/AppBars/BaseAppBar.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import { ThemeSwitcherButton, ThemeSwitcherMenuItem } from "../ThemeSwitcher";
 
 const BaseAppBar = ({ theme }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static" color={theme.palette.primary.main}>
       <Toolbar>
-        <Typography variant="h6" color={theme.palette.primary.contrastText}>
+        <Typography
+          variant="h6"
+          color={theme.palette.primary.contrastText}
+          sx={{ flexGrow: 1 }}
+        >
           WasteNotNotWant
         </Typography>
+        <div>
+          <IconButton
+            edge="edge"
+            color="inherit"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorel={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <ThemeSwitcherMenuItem />
+          </Menu>
+        </div>
+        <ThemeSwitcherButton />
       </Toolbar>
     </AppBar>
   );
