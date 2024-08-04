7f519a1ec551c5d2c81d184a2fee51cf497df06f
peppapig450 <nbgenius1@gmail.com>
2024-08-04 12:10:50 -0700

    rename "firestore" to 'db' as its more concise and better represents the purpose

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import firebaseConfig from "./config/firebaseConfig";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { firestore, auth };
+export { db, auth };
