36058cbc8c2a392424577f7e5bbcc01a07ab1976
peppapig450 <nbgenius1@gmail.com>
2024-08-03 19:36:28 -0700

    simplify the Floating action button and avoid over engineering for now

--- app/components/Pantry/PantryFloatingActionButton.jsx
+++ app/components/Pantry/PantryFloatingActionButton.jsx
@@ -1,51 +1,20 @@
 import React from "react";
-import { Fab, useTheme, Tooltip, Box, Typography } from "@mui/material";
+import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
+import { styled } from "@mui/material/styles";
 
-const PantryFloatingActionButton = ({ onClick, variant = "add" }) => {
-  const theme = useTheme();
+const StyledFab = styled(Fab)(({ theme }) => ({
+  position: "fixed",
+  bottom: theme.spacing(2),
+  right: theme.spacing(2),
+}));
 
+const FloatingActionButton = ({ onClick }) => {
   return (
-    <Tooltip
-      title={variant === "add" ? "Add Item" : "Edit Item"}
-      arrow
-      placement="left"
-      enterDelay={500}
-      leaveDelay={200}
-    >
-      <Box
-        sx={{
-          position: "fixed",
-          bottom: theme.spacing(3),
-          right: theme.spacing(3),
-          display: "flex",
-          alignItems: "center",
-          backgroundColor: theme.palette.secondary.main,
-          "&:hover": {
-            backgroundColor: theme.palette.secondary.dark,
-          },
-          borderRadius: "50px",
-          padding: theme.spacing(1),
-        }}
-      >
-        <Fab
-          color="primary"
-          aria-label={variant === "add" ? "add" : "edit"}
-          onClick={onClick}
-          sx={{ marginRight: theme.spacing(1) }}
-        >
-          {variant === "add" ? <AddIcon /> : <EditIcon />}
-        </Fab>
-        <Typography
-          variant="button"
-          sx={{ color: theme.palette.primary.contrastText }}
-        >
-          {variant === "add" ? "Add Item" : "Edit Item"}
-        </Typography>
-      </Box>
-    </Tooltip>
+    <StyledFab color="primary" aria-label="add" onClick={onClick}>
+      <AddIcon />
+    </StyledFab>
   );
 };
 
-export default PantryFloatingActionButton;
+export default FloatingActionButton;
