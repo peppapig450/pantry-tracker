f806b1be87b60b70a4e218ff4b8bb7e57a376d34
peppapig450 <nbgenius1@gmail.com>
2024-08-03 20:16:48 -0700

    move the pantry component to the pages directory where it belongs

--- app/pages/pantry.jsx
+++ app/pages/pantry.jsx
@@ -1,40 +1,52 @@
-import React, { useState, useEffect } from "react";
-import {
-  Container,
-  Box,
-  Button,
-  Card,
-  CardHeader,
-  CardContent,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  IconButton,
-  TextField,
-  createTheme,
-  ThemeProvider,
-  Menu,
-  MenuItem,
-  CircularProgress,
-} from "@mui/material";
-
-import {
-  collection,
-  getDocs,
-  getDoc,
-  setDoc,
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
-import { signOut, onAuthStateChanged } from "firebase/auth";
-import { firestore, auth } from "@/firebase";
-import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { PantryProvider } from "../components/Pantry/PantryContext";
+import PrimarySearchPantryAppBar from "../components/AppBars/PantryAppBar";
+import FloatingActionButton from "../components/Pantry/PantryFloatingActionButton";
 import PantryItemDialog from "../components/Pantry/PantryItemDialog";
 import PantryItemTable from "../components/Pantry/PantryItemTable";
-import PantryFLoatingActionButton from "../components/Pantry/PantryFloatingActionButton";
-import PrimarySearchPantryAppBar from "../components/AppBars/PantryAppBar";
+import withAuth from "../components/withAuth";
 
-// TODO: setup firestore logic for working with it and use our custom components
+const Page = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleFabClick = () => {
+    setIsEdit(false);
+    setSelectedItem(null);
+    setDialogOpen(true);
+  };
+
+  const handleEditDialogOpen = (item) => {
+    setIsEdit(true);
+    setSelectedItem(item);
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  return (
+    <PantryProvider>
+      <PrimarySearchPantryAppBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <PantryItemTable
+        searchQuery={searchQuery}
+        openEditDialog={handleEditDialogOpen}
+      />
+      <FloatingActionButton onClick={handleFabClick} />
+      <PantryItemDialog
+        open={dialogOpen}
+        handleClose={handleDialogClose}
+        item={selectedItem}
+        isEdit={isEdit}
+      />
+    </PantryProvider>
+  );
+};
+
+export default withAuth(Page);
