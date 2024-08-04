dd2506c237b1a37c3b13b83cefff1a34ba8a4349
peppapig450 <nbgenius1@gmail.com>
2024-08-03 20:46:54 -0700

    change page.js to provide all the pages (build going to fail)

--- app/page.js
+++ app/page.js
@@ -1,200 +1,37 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Stack,
-  Typography,
-  Button,
-  Modal,
-  TextField,
-  useTheme,
-} from "@mui/material";
-import { firestore } from "@/firebase";
-import {
-  collection,
-  doc,
-  getDocs,
-  query,
-  setDoc,
-  deleteDoc,
-  getDoc,
-} from "firebase/firestore";
-import PrimarySearchPantryAppBar from "./components/AppBars/PantryAppBar";
-import ThemeContextProvider from "./theme/ThemeContextProvider";
+import LandingPage from "./pages/landing";
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
+import PantryPage from "./pages/pantry";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "white",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  display: "flex",
-  flexDirection: "column",
-  gap: 3,
+import { ThemeContextProvider } from "./theme/ThemeContextProvider";
+import { usePathname } from "next/navigation";
+
+const Page = () => {
+  const pathname = usePathname();
+
+  let PageComponent;
+
+  switch (pathname) {
+    case "/signin":
+      PageComponent = SignInPage;
+      break;
+    case "/signup":
+      PageComponent = SignUpPage;
+      break;
+    case "/pantry":
+      PageComponent = PantryPage;
+      break;
+    default:
+      PageComponent = LandingPage;
+  }
+
+  return (
+    <ThemeContextProvider>
+      <PageComponent />
+    </ThemeContextProvider>
+  );
 };
 
-export default function Home() {
-  const [inventory, setInventory] = useState([]);
-  const [filteredInventory, setFilteredInventory] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [open, setOpen] = useState(false);
-  const [itemName, setItemName] = useState("");
-  const theme = useTheme();
-
-  const updateInventory = async () => {
-    const snapshot = query(collection(firestore, "inventory"));
-    const docs = await getDocs(snapshot);
-    const inventoryList = [];
-
-    docs.forEach((doc) => {
-      inventoryList.push({ name: doc.id, ...doc.data() });
-    });
-    setInventory(inventoryList);
-    setFilteredInventory(inventoryList); // XXX: only load when we search ?
-  };
-
-  useEffect(() => {
-    updateInventory();
-  }, []);
-
-  useEffect(() => {
-    // Filter inventory based on search query
-    if (searchQuery === "") {
-      setFilteredInventory(inventory);
-    } else {
-      setFilteredInventory(
-        inventory.filter(({ name }) =>
-          name.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-      );
-    }
-  }, [searchQuery, inventory]);
-
-  const addItem = async (item) => {
-    const docRef = doc(collection(firestore, "inventory"), item);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      const { quantity } = docSnap.data();
-      await setDoc(docRef, { quantity: quantity + 1 });
-    } else {
-      await setDoc(docRef, { quantity: 1 });
-    }
-    await updateInventory();
-  };
-
-  const removeItem = async (item) => {
-    const docRef = doc(collection(firestore, "inventory"), item);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      const { quantity } = docSnap.data();
-      if (quantity === 1) {
-        await deleteDoc(docRef);
-      } else {
-        await setDoc(docRef, { quantity: quantity - 1 });
-      }
-    }
-    await updateInventory();
-  };
-
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  // Add component logic here
-  return (
-    <>
-      <PrimarySearchPantryAppBar
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-      <Box
-        width="100vw"
-        height="100vh"
-        display={"flex"}
-        justifyContent={"center"}
-        flexDirection={"column"}
-        alignItems={"center"}
-        gap={2}
-        bgcolor={theme.palette.background.default}
-      >
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Add Item
-            </Typography>
-            <Stack width="100%" direction={"row"} spacing={2}>
-              <TextField
-                id="outlined-basic"
-                label="Item"
-                variant="outlined"
-                fullWidth
-                value={itemName}
-                onChange={(e) => setItemName(e.target.value)}
-              />
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  addItem(itemName);
-                  setItemName("");
-                  handleClose();
-                }}
-              >
-                Add
-              </Button>
-            </Stack>
-          </Box>
-        </Modal>
-        <Button variant="contained" onClick={handleOpen}>
-          Add New Item
-        </Button>
-        <Box border={`1px solid ${theme.palette.divider}`}>
-          <Box
-            width="800px"
-            height="100px"
-            bgcolor={"#ADD8E6"}
-            display={"flex"}
-            justifyContent={"center"}
-            alignContent={"center"}
-          >
-            <Typography variant={"h2"} color={"#333"} textAlign={"center"}>
-              Inventory Items
-            </Typography>
-          </Box>
-          <Stack width="800px" height="300px" spacing={2} overflow={"auto"}>
-            {filteredInventory.map(({ name, quantity }) => (
-              <Box
-                key={name}
-                width="100%"
-                height="150px"
-                display={"flex"}
-                justifyContent={"space-between"}
-                alignItems={"center"}
-                bgcolor={"#f0f0f0"}
-                paddingX={5}
-              >
-                <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
-                  {name.charAt(0).toUpperCase() + name.slice(1)}
-                </Typography>
-                <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
-                  Quantity: {quantity}
-                </Typography>
-                <Button variant="contained" onClick={() => removeItem(name)}>
-                  Remove
-                </Button>
-              </Box>
-            ))}
-          </Stack>
-        </Box>
-      </Box>
-    </>
-  );
-}
+export default Page;
