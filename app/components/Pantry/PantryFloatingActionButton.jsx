572989123547bd86821cd1d9c353e89f696542b5
peppapig450 <nbgenius1@gmail.com>
2024-08-04 10:43:34 -0700

    try rewriting Fab button

--- app/components/Pantry/PantryFloatingActionButton.jsx
+++ app/components/Pantry/PantryFloatingActionButton.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
-import { styled } from "@mui/material/styles";
 import { useTheme } from "@mui/material/styles";
-
-const StyledFab = styled(Fab)(({ theme }) => ({
-  position: "fixed",
-  bottom: theme.spacing(2),
-  right: theme.spacing(2),
-}));
 
 const FloatingActionButton = ({ onClick }) => {
   const theme = useTheme();
 
   return (
-    <StyledFab theme={theme} aria-label="add" onClick={onClick}>
+    <Fab
+      aria-label="add"
+      onClick={onClick}
+      sx={{
+        position: "fixed",
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+      }}
+    >
       <AddIcon />
-    </StyledFab>
+    </Fab>
   );
 };
 
